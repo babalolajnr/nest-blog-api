3b79b9867ca5723b68a9ee127a83d54895d17a86
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-09-17 23:08:52 +0100

    create posts module

---
+++ src/posts/posts.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsController } from './posts.controller';
+import { Post } from './posts.entity';
+import { PostsService } from './posts.service';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Post])],
+    providers: [PostsService],
+    controllers: [PostsController],
+    exports: [TypeOrmModule]
+})
+export class PostsModule {}
