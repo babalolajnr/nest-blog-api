c4cb6e9cc9fda9b05ee5b2c10e0173b2b3045a07
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-09-17 15:34:23 +0100

    add exports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,5 +8,6 @@ import { User } from './user.entity';
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [TypeOrmModule]
 })
 export class UsersModule {}
