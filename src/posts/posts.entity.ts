3a9a3b9d62713d6dbab0750af71bb86d35f79cee
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-09-17 16:20:06 +0100

    create posts entity

---
+++ src/posts/posts.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Post{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    content: string;
+
+    @Column()
+    published: boolean;
+
+    
+}
