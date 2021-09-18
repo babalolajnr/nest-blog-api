c0905461b2106a9d77515891250c5e080887f821
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-09-18 19:41:55 +0100

    add create method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
-@Controller('posts')
-export class PostsController {}
+@Controller('post')
+export class PostsController {
+
+    constructor(private readonly postsService: PostsService) { }
+
+    @Post('create')
+    async create(@Body() values: { title: string, content: string }): Promise<any> {
+
+        const post = await this.postsService.create(values.title, values.content)
+
+        return post
+    }
+}
