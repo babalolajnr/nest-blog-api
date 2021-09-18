d14205dd8ff9f01499d1ac351fb4850a775e691c
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-09-18 19:40:34 +0100

    add create method

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './posts.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    constructor(@InjectRepository(Post) private postsRepository: Repository<Post>) { }
+
+    async create(title: string, content: string): Promise<Post> {
+
+        const newPost = this.postsRepository.create({ title, content });
+
+        return this.postsRepository.save(newPost);
+    }
+}
