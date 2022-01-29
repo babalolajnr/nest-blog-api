4564fef9b8318945b8c0f47ea3429bfcf8b2adb8
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-01-29 18:49:32 +0100

    use createUserDto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('user')
@@ -6,20 +7,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('register')
-  async register(
-    @Body()
-    values: {
-      firstName: string;
-      lastName: string;
-      email: string;
-      password: string;
-    },
-  ): Promise<void> {
-    this.usersService.register(
-      values.firstName,
-      values.lastName,
-      values.email,
-      values.password,
-    );
+  async register(@Body() createUserDto: CreateUserDto): Promise<void> {
+    this.usersService.register(createUserDto);
   }
 }
