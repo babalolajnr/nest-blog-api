c3fbb1460e98aeefbbe3e1bb8f767838c167f397
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-01-29 18:50:09 +0100

    format

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -5,12 +5,13 @@ import { Post } from './posts.entity';
 
 @Injectable()
 export class PostsService {
-    constructor(@InjectRepository(Post) private postsRepository: Repository<Post>) { }
+  constructor(
+    @InjectRepository(Post) private postsRepository: Repository<Post>,
+  ) {}
 
-    async create(title: string, content: string): Promise<Post> {
+  async create(title: string, content: string): Promise<Post> {
+    const newPost = this.postsRepository.create({ title, content });
 
-        const newPost = this.postsRepository.create({ title, content });
-
-        return this.postsRepository.save(newPost);
-    }
+    return this.postsRepository.save(newPost);
+  }
 }
