d84d50d96b3b6996c277cab7b955f953ffb979e9
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-01 07:02:37 +0100

    add JwtAuthGuard

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,16 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreatePostDto } from './create-post.dto';
 import { PostsService } from './posts.service';
 
 @Controller('post')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('create')
-  async create(
-    @Body() values: { title: string; content: string },
-  ): Promise<any> {
-    const post = await this.postsService.create(values.title, values.content);
-
+  async create(@Body() createPostDto: CreatePostDto): Promise<any> {
+    const post = await this.postsService.create(createPostDto);
     return post;
   }
 }
