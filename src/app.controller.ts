f3edd218be22420a5a2a7422dc8dbb3f25c22a89
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-01 01:20:40 +0100

    use auth service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,14 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
