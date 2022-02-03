43de6a89291d326481ff849ca2c7ceae01ee75d4
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-03 22:51:36 +0100

    not working yet

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { getRepository, Repository } from 'typeorm';
 import { CreatePostDto } from './create-post.dto';
 import { Post } from './posts.entity';
 
@@ -10,9 +11,26 @@ export class PostsService {
     @InjectRepository(Post) private postsRepository: Repository<Post>,
   ) {}
 
-  async create(post: CreatePostDto): Promise<Post> {
-    return this.postsRepository.save(post).catch((e) => {
-      return e;
+  async create(post: CreatePostDto, user): Promise<any> {
+    const userRepository = getRepository(User);
+
+    const author = await userRepository.findOne({
+      where: { email: 'babalolajnr@gmail.com' },
     });
+
+    const newPost: Post = new Post();
+
+    newPost.content = post.content;
+    newPost.title = post.title;
+    newPost.user = author;
+
+    return this.postsRepository
+      .save(post)
+      .catch((e) => {
+        return e;
+      })
+      .then((post) => {
+        return post;
+      });
   }
 }
