188d50e1300af01a762315ca923be9030c12a35a
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-03 22:50:06 +0100

    get user sending the request with RequestWithUser

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreatePostDto } from './create-post.dto';
 import { PostsService } from './posts.service';
-
+import { RequestWithUser } from '../users/request-with-user';
 @Controller('post')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('create')
-  async create(@Body() createPostDto: CreatePostDto): Promise<any> {
-    const post = await this.postsService.create(createPostDto);
+  async create(
+    @Body() createPostDto: CreatePostDto,
+    @Req() request: RequestWithUser,
+  ): Promise<any> {
+    const post = await this.postsService.create(createPostDto, request.user);
     return post;
   }
 }
